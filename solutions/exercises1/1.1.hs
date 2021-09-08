d6e8bc508609cbdb03fe0ab41401b0c220ffecef
jelmervanvugt <JA.vanVugt@student.han.nl>
2021-09-08 16:08:51 +0200

    excercise 1.1

---
+++ solutions/exercises1/1.1.hs
@@ -0,0 +1,21 @@
+{-
+    i prevented 0 and negative seat numbers
+    bug: for some reason the newlines arent being printed correctly
+-}
+
+lyrics :: Integer -> String
+
+lyrics n
+    | n < 0 = "negative numbers are not allowed"
+    | n == 0 = "no more seats in the lecture hall"
+    | otherwise = show n ++ " " ++ seats ++ " in the lecture hall! " ++
+        "Only " ++ show n ++ " " ++ seats ++ " left!\n" ++
+        "A students walks in, and sits down, now there are\n" ++ lyrics (n-1)
+            where seats =   if n /= 1 then "seats"
+                            else "seat"
+
+song :: String
+song = lyrics (-5)
+
+main = putStr("output of fn \n" ++ song)
+
