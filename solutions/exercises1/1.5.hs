dbf94a4dd0e88468e4e88bd7d25a3b5a229ffc37
jelmervanvugt <JA.vanVugt@student.han.nl>
2021-09-09 13:43:35 +0200

    finished ex 1.5

--- solutions/exercises1/1.5.hs
+++ solutions/exercises1/1.5.hs
@@ -4,17 +4,18 @@ type FavouriteCourse  = String
 
 type Person           = (Name, Age, FavouriteCourse)
 
-elena, peter, pol :: Person
+elena, peter, pol, frits :: Person
 elena   = ("Elena", 33, "Functional Programming")
 peter   = ("Peter", 33, "Imperative Programming")
 pol     = ("Pol", 36, "Quantum Field Theory")
+frits   = ("Frits", 38, "Operating Systems")
 
 -- 1
 jelmer  = ("Jelmer", 4, "The Main Course")
 eric    = ("Eric", 42, "Yet unknown")
 
 students :: [Person]
-students = [elena, peter, pol]
+students = [elena, peter, pol, frits, jelmer, eric]
 
 -- 2
 age :: Person -> Age
@@ -44,10 +45,14 @@ increaseAge p = (name p, (age p) + 1, favouriteCourse p)
 -- Testing of functions can be done by calling main in GHCi paired with the right function
 -- !! n gets the Nth value in the list, in this case the first one
 
+--main = putStr (favouriteCourse(students !! 0))
+
 {- increment the age of all students by two -}
 -- (map increaseAge (map increaseAge students))
 
 {- promote all of the students -}
+promote :: Person -> Person
+promote p = (("dr. " ++ name p), age p, favouriteCourse p)
 -- map promote students
 
 {- find all students named Frits -}
@@ -56,7 +61,15 @@ increaseAge p = (name p, (age p) + 1, favouriteCourse p)
 {- find all students who are in their twenties -}
 -- let inTwenties p = (age p) >= 20 && (age p) < 30 in filter inTwenties students
 
---main = putStr (favouriteCourse(students !! 0))
+{- compute the average age of all students -}
+averageAge s = (fromIntegral(sum (map age s)) `div` (length s))
+
+{- promote all students whose favourite course is functional programming -}
+promoteIfFavourite :: Person -> Person
+promoteIfFavourite s =
+    if fpFav then promote s else s
+    where fpFav = favouriteCourse s == "Functional Programming"
+--main = putStr(show (map promoteIfFavourite students))
 
 
 
