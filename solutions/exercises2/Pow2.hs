6ac73c42f92a330213df12213d6f5952f8f3a095
Eric Teunis de Boone <ericteunis@deboone.nl>
2021-09-15 15:08:46 +0200

    Wk2: Ex2 and 3(Needs work)

--- solutions/exercises2/Pow2.hs
+++ solutions/exercises2/Pow2.hs
@@ -1,9 +1,16 @@
 module Pow2 where
 
-pow2 :: Integer -> Integer
+pow2 :: (Ord n, Num n, Num a) => n -> a
 pow2 0 = 1
--- pow2 n = ... recursive case
+-- pow2 n = 2 * (pow2 (n-1))
+pow2 n
+ | n < 1 = 2 ^^ fromIntegral(n)
+ | otherwise = 2 * (pow2 (n-1))
 
-
---after finishing the function, change the type to: 
---pow2 :: (Ord n, Num n, Num a) => n -> a
+{- Type    | Maximum n
+ - -------------------
+ - Integer | \infty
+ - Int     | 62 (pow2 63 is negative)
+ - Float   | 127
+ - Double  | 1023
+-}
