dcbeb304e9e6490119e023e2dc4de1ca791b9c26
Eric Teunis de Boone <ericteunis@deboone.nl>
2021-09-20 08:32:43 +0200

    Wk2: finalised 2.5

--- solutions/exercises2/2.5/Char.hs
+++ solutions/exercises2/2.5/Char.hs
@@ -1,25 +1,30 @@
 module Char where
 
 import Data.Char
-import Distribution.Simple.Utils (xargs)
 
 -- 1
-(~~) :: String -> String -> Bool 
+(~~) :: String -> String -> Bool
 (~~) x y = map toUpper x == map toUpper y
 
 -- 2
 reverseCase :: String -> String
-reverseCase = map (\x -> if isUpper x then toLower x else toUpper x) 
+reverseCase = map (\x -> if isUpper x then toLower x else toUpper x)
 
 -- 3
 shift :: Int -> Char -> Char
-shift x y = if isUpper y then (chr.(+x).ord) y else y
+shift n x = if not (isAsciiUpper x) then x else shiftChar x
+  where shiftChar y = chr ( ord 'A' + (ord y - ord 'A' + n ) `mod` ( 1 + ord 'Z' - ord 'A'))
 
--- 4 (testcases do not seem to work yet, but when testing manually is works fine)
+-- 4
 caesar :: Int -> String -> String
 caesar x y = map (shift x) z
-    where z = map toUpper y    
+    where z = map toUpper y
 
-
+-- 5
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
+{- If this is english, the 'D' should either be 'A' or 'I'
+ - corresponding with a shift of -3 or +5.
+ -
+ - (caesar 5 msg) gives something about fairy dust sprinkling.
+ -}
